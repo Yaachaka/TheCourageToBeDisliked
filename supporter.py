2ed0fb89d8b37fb309e9352cd78265e3bf5b4476
Yaachaka <rosaarjuna@gmail.com>
2023-08-19 17:31:48 +0530

    Chapter03p01 completed.

--- supporter.py
+++ supporter.py
@@ -4,7 +4,8 @@ with open("supporter.txt", "r") as file1:
 	for line in file1:
 		arr1.append(line)
 
-# print("<div class=\"chapter_02\" id=\"id-chapter_02\">")
+print("\t<hr class=\"contentDivide\">")
+print("\t<div class=\"chapter_03\" id=\"id-chapter_03\">")
 arr1_len = len(arr1)
 for i in range(arr1_len):
 	pos = arr1[i].index(": ")
@@ -12,8 +13,9 @@ for i in range(arr1_len):
 	if "HEADER2: " in arr1[i]:
 		print("\t<h2>" + var1 + "</h2>")
 	elif "HEADER3: " in arr1[i]:
+		print("\t</div>")
 		print("\t<hr class=\"contentDivide\">")
-		print("\t<div class=\"chapter_02p10\" id=\"id-chapter_02p10\">")
+		print("\t<div class=\"chapter_03p01\" id=\"id-chapter_03p01\">")
 		print("\t\t<h3>" + var1 + "</h3>")
 	elif "NARRATOR: " in arr1[i]:
 		print("\t\t<div class=\"narrator\">")
@@ -30,4 +32,3 @@ for i in range(arr1_len):
 		print("\t\t\t<p>" + var1 + "</p>")
 		print("\t\t</div>")
 print("\t</div>")
-# print("</div>")
